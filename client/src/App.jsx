c3817a8d9d9e20068aabda8304263dd8f8aeba37
pritkhokhani70 <pritkhokhani70@gmail.com>
2024-04-21 15:21:04 +0530

    create react router create and run this server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter ,Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home';
+import Signin from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import Headers from './Component/Header.jsx'
+
 
 function App() {
 
 
   return (
     <>
-      <h1 className=''>real-estate</h1>
+        <BrowserRouter>
+          <Headers />
+          <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/sign-in' element={<Signin></Signin>}></Route>
+            <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
+            <Route path='/about' element={<About></About>}></Route>
+            <Route path='/profile' element={<Profile></Profile>}></Route>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
