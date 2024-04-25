0ce5e8f0d4d470eb2afe73ee93e058cc14547d7a
pritkhokhani70 <pritkhokhani70@gmail.com>
2024-04-26 00:44:03 +0530

    -- connect to databse

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("connect database");
+})
+.catch((err) => {
+    console.log(err);
+})
 
 app.listen(3000, () => {
     console.log('server running');
